85959df4a483a3fd6b2587bf3c442a72f57becd4
Ali-Saad <V16ASaad10@vodafone.com>
2021-12-06 14:24:39 +0200

    update config.yml

--- backend/src/modules/config/config.service.ts
+++ backend/src/modules/config/config.service.ts
@@ -1,7 +1,7 @@
 import * as Joi from '@hapi/joi';
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { AboutInfo } from './about.interface';
-//import { Product } from '../../modules/domain/orders/entities/product.entity';
+import { Product } from '../../modules/domain/orders/entities/product.entity';
 import { Order } from '../../modules/domain/orders/entities/order.entity';
 import { Employee } from '../domain/employees/entities/employee.entity';
 
@@ -103,8 +103,8 @@ export class ConfigService {
       username: this.envConfig.TYPEORM_USERNAME,
       password: this.envConfig.TYPEORM_PASSWORD,
       database: this.envConfig.TYPEORM_DATABASE,
-      entities: [this.envConfig.TYPEORM_ENTITIES],
-      // entities: [Product, Order, Employee],
+      //entities: [this.envConfig.TYPEORM_ENTITIES],
+      entities: [Product, Order, Employee],
       logging: this.envConfig.TYPEORM_LOGGING === 'true',
       extra: { max: 4, min: 1 },
       synchronize: false,
